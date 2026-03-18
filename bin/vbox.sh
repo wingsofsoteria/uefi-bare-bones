9a8187006e8d12d8cca869dc1604814fba7cdbd7
Soteria <voidmeridian@gmail.com>
2026-03-17 22:32:55 -0400

    modified vbox script

--- bin/vbox.sh
+++ bin/vbox.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 trap "exit 1" SIGINT
 make clean && make && sudo make images
-VBoxManage convertfromraw --format vdi fat.img "test.vdi"
+rm test.vdi
+VBoxManage convertfromraw --format vdi fat.img "test.vdi" --uuid "472fac57-cc3a-4469-a101-8ef268eaac6b"
