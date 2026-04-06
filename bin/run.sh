0acb32c05f9608acacd005e016acbea49f0d98fc
Soteria <voidmeridian@gmail.com>
2026-04-06 09:03:54 -0400

    changed run.sh to use makefile more directly

--- bin/run.sh
+++ bin/run.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 trap "exit 1" SIGINT
-export KERNEL_DEBUG=1
-bin/apply.sh
+export FEATURE_DEBUG=1
+export FEATURE_LIMINE=1
+make clean
+make
+make images
+make qemu
 # ./elf_tester
-qemu-system-x86_64 -accel kvm -cpu host,+invtsc -m 4G -drive if=pflash,format=raw,readonly=on,file=bin/OVMF.fd -drive format=raw,file=fat.img  -net none -no-reboot -debugcon stdio  2> >(tee log >&2)
