1e8de442cc6be397400a7babed85a1afca0b637a
Soteria <voidmeridian@gmail.com>
2026-04-26 15:05:44 -0400

    added some paging flags

--- kernel/include/memory/paging.h
+++ kernel/include/memory/paging.h
@@ -2,5 +2,9 @@
 #include <stdint.h>
 #define PAGE_SIZE  4096
 #define PAGE_COUNT 512
+
+#define PAGE_WRITABLE (1 << 1)
+#define PAGE_USER     (1 << 2)
 void unmap_page(uint64_t virt);
+uint64_t virtual_to_physical(uint64_t);
 void map_page(uint64_t virt, uint64_t phys, uint16_t flags);
